27863e3afc12c8d3badbe1b1d96d98fd616db2c6
jerem <jeremie.prouteau75@gmail.com>
2023-07-08 22:34:31 +0200

    testtest

--- js/pagination.js
+++ js/pagination.js
@@ -1,67 +1,81 @@
-// Constants
-const articlesPerPage = 5; // Maximum number of articles per page
+const articlesPerPage = 6; // Maximum number of articles per page
+const masonryContainer = document.querySelector('.bricks-wrapper');
+const articles = [...document.querySelectorAll('.brick')]; // Array of <article> elements
 
-// Initial data
-let articles = []; // Array to store articles
+let currentPage = 1;
+let masonry; // Masonry instance
 
-// Add articles to the system
-function addArticle(title) {
-  articles.push({ title });
-  updatePagination();
-}
-
-// Update pagination
 function updatePagination() {
-  const totalArticles = articles.length; // Total number of articles
-  let totalPages = Math.ceil(totalArticles / articlesPerPage); // Total number of pages
-
-  const currentPage = getCurrentPage(); // Get the current page
   const startIndex = (currentPage - 1) * articlesPerPage;
   const endIndex = currentPage * articlesPerPage;
-  const articlesToDisplay = articles.slice(startIndex, endIndex);
+  const visibleArticles = articles.slice(startIndex, endIndex);
 
-  renderPagination(articlesToDisplay, totalPages, currentPage);
-  renderArticles(articlesToDisplay);
-}
-
-// Get the current page
-function getCurrentPage() {
-  // Logic to determine the current page
-  // ...
-}
-
-// Render pagination UI
-function renderPagination(articlesToDisplay, totalPages, currentPage) {
-  // Example rendering logic
-  console.clear();
-  console.log(`Current Page: ${currentPage}`);
-  console.log(`Total Pages: ${totalPages}`);
-  console.log(`Total Articles: ${articles.length}`);
-
-  // Update pagination controls in the HTML if needed
-  // ...
-}
-
-// Render articles in the <article> container
-function renderArticles(articlesToDisplay) {
-  const articleContainer = document.getElementById('article-container');
-
-  // Clear existing articles
-  articleContainer.innerHTML = '';
-
-  // Create and append new article elements
-  articlesToDisplay.forEach((article, index) => {
-    const articleElement = document.createElement('article');
-    articleElement.textContent = `${index + 1}. ${article.title}`;
-    articleContainer.appendChild(articleElement);
+  // Hide all articles
+  articles.forEach((article) => {
+    article.style.display = 'none';
   });
+
+  // Show visible articles
+  visibleArticles.forEach((article) => {
+    article.style.display = 'block';
+  });
+
+  // Update pagination UI (e.g., active page indicator, disable/enable buttons)
+  const pageNumberElement = document.getElementById('page-number');
+  pageNumberElement.textContent = currentPage.toString();
+
+  // Initialize or reload the Masonry layout
+  if (masonry) {
+    masonry.reloadItems();
+    masonry.layout();
+  } else {
+    masonry = new Masonry(masonryContainer, {
+      itemSelector: '.brick',
+      columnWidth: '.grid-sizer',
+      percentPosition: true,
+      transitionDuration: 0,
+    });
+  }
 }
+
+function nextPage() {
+  const totalPages = getTotalPages();
+  if (currentPage < totalPages) {
+    currentPage++;
+    updatePagination();
+    onPageChange(currentPage); // Call the callback function on page change
+  }
+}
+
+function previousPage() {
+  if (currentPage > 1) {
+    currentPage--;
+    updatePagination();
+    onPageChange(currentPage); // Call the callback function on page change
+  }
+}
+
+function getTotalPages() {
+  return Math.ceil(articles.length / articlesPerPage);
+}
+
+// Callback function for page change
+function onPageChange(page) {
+  // Access the newly created page here
+  console.log(`Accessing page ${page}`);
+}
+
+// Event listener for "Next Page" button click
+const nextPageButton = document.getElementById('next-page-button');
+nextPageButton.addEventListener('click', () => {
+  nextPage();
+});
+
+// Event listener for "Previous Page" button click
+const previousPageButton = document.getElementById('previous-page-button');
+previousPageButton.addEventListener('click', () => {
+  previousPage();
+});
 
 // Usage example
-addArticle("Article 1");
-addArticle("Article 2");
-addArticle("Article 3");
-addArticle("Article 4");
-addArticle("Article 5");
-addArticle("Article 6");
-addArticle("Article 7");
+updatePagination();
